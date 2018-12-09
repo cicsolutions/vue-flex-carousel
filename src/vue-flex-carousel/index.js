475a104c6ee53ed7bf523aa69720fedc4d955c79
Caleb Crosby <caleb.crosby@cicsolutions.com>
2018-12-09 16:32:48 -0500

    update file indents

--- src/vue-flex-carousel/index.js
+++ src/vue-flex-carousel/index.js
@@ -2,15 +2,15 @@ import FlexCarousel from './components/_CarouselInput.vue'
 import Slide from './components/_SlideInput.vue'
 
 const install = (Vue) => {
-    Vue.component('flex-carousel', FlexCarousel)
-    Vue.component('slide', Slide)
+  Vue.component('flex-carousel', FlexCarousel)
+  Vue.component('slide', Slide)
 }
 
 export default {
-    install
+  install
 }
 
 export {
-    FlexCarousel,
-    Slide
+  FlexCarousel,
+  Slide
 }
