a590fff98d1869df64b993cc5c233f864ec57645
Caleb Crosby <caleb.crosby@cicsolutions.com>
2018-12-09 16:29:09 -0500

    Create empty _input comonents - these will be used by the consumer and ultimately pass the carousel/slide data to our rendering components

---
+++ src/vue-flex-carousel/components/_CarouselInput.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>carousel input</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/vue-flex-carousel/components/_SlideInput.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>slide input</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+</style>
