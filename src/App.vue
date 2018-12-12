3c1224c477b18d00a0d80d21d9315c2176513c44
Caleb Crosby <caleb.crosby@cicsolutions.com>
2018-12-12 08:18:30 -0500

    Rebuild lost files

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <div class="flex justify-between items-center p-3 bg-grey-lighter shadow border-b fixed w-full">
+      <img alt="Vue logo" src="./assets/logo.png" class="w-full" style="max-width: 50px;">
+      <h1 class="">Vue Flex Carousel Sandbox</h1>
+      <img alt="Vue logo" src="./assets/logo.png" class="w-full invisible" style="max-width: 50px;">
+    </div>
+
+    <div class="h-full flex items-center justify-center">
+      <flex-carousel
+
+        :slides="carouselSlides"
+      >
+
+      </flex-carousel>
+    </div>
+
   </div>
 </template>
 
 <script>
-
+import './assets/app.scss'
+import { FlexCarousel } from './vue-flex-carousel'
 export default {
   name: 'app',
   components: {
+    FlexCarousel
+  },
+  computed: {
+    carouselSlides() {
+      return [
+        {
+          title: 'Slide #',
+          caption: 'My amazing slide caption.',
+          src: 'https://placeimg.com/640/480/nature',
+          bgColor: '',
+          bgImageUrl: '',
+          innerHtml: '',
+          order: null, // CICS TODO: need to decide how to handle values from user that are not accurate
+          isVisible: true
+        },
+      ]
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
